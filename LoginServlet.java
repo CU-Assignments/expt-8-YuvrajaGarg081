a63bbf5b9ea915c9aeee83f518f0272abd2ff394
Yuvraj Garg <110448058+YuvrajaGarg081@users.noreply.github.com>
2025-04-15 15:05:25 +0530

    LoginServlet

---
+++ LoginServlet.java
@@ -0,0 +1,34 @@
+import java.io.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
+public class LoginServlet extends HttpServlet {
+    private final String validUsername = "admin";
+    private final String validPassword = "password123";
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+        if (validUsername.equals(username) && validPassword.equals(password)) {
+            out.println("<html><body>");
+            out.println("<h2>Welcome, " + username + "!</h2>");
+            out.println("</body></html>");
+        } else {
+            out.println("<html><body>");
+            out.println("<h2>Login failed. Invalid username or password.</h2>");
+            out.println("</body></html>");
+        }
+        out.close();
+    }
+}
+<web-app>
+    <servlet>
+        <servlet-name>LoginServlet</servlet-name>
+        <servlet-class>LoginServlet</servlet-class>
+    </servlet>
+    <servlet-mapping>
+        <servlet-name>LoginServlet</servlet-name>
+        <url-pattern>/LoginServlet</url-pattern>
+    </servlet-mapping>
+</web-app>
